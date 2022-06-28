8fd0b9e085ac59050c28c02774164287c0ce50e3
KimVianney <kimvianney6@gmail.com>
2022-06-28 16:56:25 +0300

    Create update account, resend email verification mutations

--- users/schema.py
+++ users/schema.py
@@ -8,3 +8,5 @@ class AuthMutations(graphene.ObjectType):
     register = mutations.Register.Field()
     verify_account = mutations.VerifyAccount.Field()
     token_auth = mutations.ObtainJSONWebToken.Field()
+    update_account = mutations.UpdateAccount.Field()
+    resend_activation_email = mutations.ResendActivationEmail.Field()
