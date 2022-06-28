2072650d568a05513b4f119b8f93d9b4e85dbde3
KimVianney <kimvianney6@gmail.com>
2022-06-28 16:20:08 +0300

    Verify user and allow login

--- users/schema.py
+++ users/schema.py
@@ -6,3 +6,5 @@ from graphql_auth import mutations
 
 class AuthMutations(graphene.ObjectType):
     register = mutations.Register.Field()
+    verify_account = mutations.VerifyAccount.Field()
+    token_auth = mutations.ObtainJSONWebToken.Field()
